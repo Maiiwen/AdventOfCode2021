01c50abb9b70a8ace56c40692f10b6f2abaeb37f
Maiwen <43137876+Maiiwen@users.noreply.github.com>
2021-12-04 16:51:37 +0100

    part 2 day 3 finished

--- day3/part1.py
+++ day3/part1.py
@@ -1,18 +1,18 @@
 from splitInputToList import openFileToTab
 inputValues = openFileToTab()
-del inputValues[(len(inputValues)-1)]
 lenValues = len(inputValues)
 count = [0] * len(inputValues[0])
-gamma = [0] * len(inputValues[0])
-epsilon = [0] * len(inputValues[0])
+gamma = ""
+epsilon = ""
 for i in range(len(inputValues)):
     entry = str(inputValues[i])
-    for i in range(len(entry)):
-        if entry[i] == "1":
-            count[i] += 1
+    for j in range(len(entry)):
+        if entry[j] == "1":
+            count[j] += 1
 for i in range(len(entry)):
-    gamma[i] = 1 if count[i] > (lenValues//2) else 0 
-    epsilon[i] = 0 if count[i] > (lenValues//2) else 1
+    gamma += "1" if count[i] > (lenValues//2) else "0" 
+    epsilon += "0" if count[i] > (lenValues//2) else "1"
 
-print(count)
-print(gamma,epsilon)
+
+print(int(gamma,2),int(epsilon,2))
+print(int(gamma,2)*int(epsilon,2))
